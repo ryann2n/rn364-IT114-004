6ce9911053406445fd8401b087464fb31ff7c45c
ryann2n <rn364@njit.edu>
2024-02-12 21:16:53 -0500

    NumberGuesser4.java

--- Problem1.java
+++ Problem1.java
@@ -17,8 +17,7 @@ public class Problem1 {
         System.out.println("Processing Array:" + Arrays.toString(arr));
         System.out.println("Odds output:");
         //hint: use the arr variable; don't diretly use the a1-a4 variables
-        //TODO add/edit code here
-        //rn364 2-6-24
+        //TODO add/edit code here //rn364 2-6-24
         for (int num : arr){
             if (num % 2 != 0){
                 System.out.print(num + "");
